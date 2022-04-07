fca6c98f8dcd0454d6508a347464255c3eb53f87
Cameron Barker <cameronbarkeran@gmail.com>
2022-04-07 16:27:18 +0100

    struct print

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -12,14 +12,20 @@ fn main() {
     //     area(rect1)
     // );
 
+    // let rect1 = Rectangle {
+    //     width: 30,
+    //     height: 50,
+    // };
+    // println!(
+    //     "The area of the rectangle is {} square pixels.",
+    //     area(&rect1)
+    // );
+
     let rect1 = Rectangle {
         width: 30,
         height: 50,
     };
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        area(&rect1)
-    );
+    println!("rect1: {:#?}", rect1);
 }
 
 // fn area(width: u32, height: u32) -> u32 {
@@ -30,11 +36,17 @@ fn main() {
 //     dimensions.0 * dimensions.1
 // }
 
+// struct Rectangle {
+//     width: u32,
+//     height: u32,
+// }
+
+// fn area(rectangle: &Rectangle) -> u32 {
+//     rectangle.width * rectangle.height
+// }
+
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
-}
-
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
 }
